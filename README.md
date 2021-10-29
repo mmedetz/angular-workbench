2175b87ef9ce6a0699ee5b26607aa948730f99d3
mmedetz <maximilian.medetz@gmail.com>
2021-10-29 14:17:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-kw8v9i
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-kw8v9i)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, FlexLayoutModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
